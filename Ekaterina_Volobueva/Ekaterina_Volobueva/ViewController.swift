a788dc6ddb730aa69c53deeb79c274f65ecbbde7
Екатерина Волобуева <>
2024-03-18 14:10:25 +0300

    new project started

---
+++ Ekaterina_Volobueva/Ekaterina_Volobueva/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Ekaterina_Volobueva
+//
+//  Created by Ekaterina Volobueva on 18.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .brown
+    }
+}
+
