ce0c9a672a81496b8ec5e2a2c040b67ca4595c5c
Екатерина Волобуева <>
2024-04-25 09:10:35 +0300

    storyboard deleted

---
+++ architecture/architecture/MainViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MainViewController.swift
+//  architecture
+//
+//  Created by Ekaterina Volobueva on 25.04.2024.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemMint
+    }
+}
